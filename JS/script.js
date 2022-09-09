350a976f0deff4699a357f3f77fc99ec54ec1d86
Igor Guimarães <igorgui.tec@gmail.com>
2022-09-09 01:07:53 -0300

    dinamicidade do botão

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,49 @@
 const url = 'https://mock-api.driven.com.br/api/v4/shirts-api/shirts';
-const promessa = axios.get(url);
-promessa.then(console.log(promessa));
+const itens = [];
 
+const linkImg = document.querySelector('.linkImg');
+const confirmBtn = document.querySelector('.confirmBtn');
+const allOptions = document.querySelectorAll('.options');
 
+function buscarItens() {
+    const promessa = axios.get(url);
+    promessa.then(renderizar);
+}
+buscarItens()
+
+function renderizar(resposta) {
+    const ArrayItens = resposta.data;
+    ArrayItens.forEach(element => {
+        itens.push(element);
+    });
+    console.log(itens);
+}
 
 function select(objToSelect) {
     const gFatherOption = objToSelect.parentNode.parentNode;
     const brothers = gFatherOption.querySelectorAll('.circle');
 
-    for(let i = 0; i < brothers.length; i++){
-        if(brothers[i].classList.contains('selected')){
+    for (let i = 0; i < brothers.length; i++) {
+        if (brothers[i].classList.contains('selected')) {
             brothers[i].classList.remove('selected');
         }
+        
     }
+    
     objToSelect.classList.add('selected');
 
 }
 
-/* let filledLink = document.querySelector('.linkImg').startsWith('https://');
+function olar(){
+    console.log("olar");
+}
 
-if(filledLink){
-    document.querySelector('.confirmBtn').classList.add('filled')
-} */
+function coloredbutton() {
+    const selecteds = document.querySelectorAll('.selected');
+    if(linkImg.value.startsWith('https://') && selecteds.length === allOptions.length){
+        confirmBtn.classList.add('filled');
+        confirmBtn.setAttribute("onclick", "olar()")
+    }
+}
+
+linkImg.addEventListener("keydown", coloredbutton);
